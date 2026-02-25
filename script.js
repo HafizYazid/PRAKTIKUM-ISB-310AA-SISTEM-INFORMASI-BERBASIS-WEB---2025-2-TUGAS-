3e4bad5a630454026f2af18b80a2fafac08d7f67
haris <hafizyazid.nst@harisnst.local>
2026-02-25 15:44:10 +0700

    up tugas minggu 2

---
+++ script.js
@@ -0,0 +1,50 @@
+const btnTheme = document.getElementById('btn-theme');
+const body = document.body;
+
+// Cek apakah ada simpanan tema di browser?
+if (localStorage.getItem('theme') === 'dark') {
+    body.classList.add('dark-mode');
+    btnTheme.innerText = "☀️ Mode Terang";
+}
+
+btnTheme.addEventListener('click', function() {
+    body.classList.toggle('dark-mode');
+
+    if (body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark');
+        btnTheme.innerText = "☀️ Mode Terang";
+    } else {
+        localStorage.removeItem('theme');
+        btnTheme.innerText = "🌙 Mode Gelap";
+    }
+});
+
+// ===========================================
+// 2. FITUR BELI (Event Listener & Math)
+// ===========================================
+function aktifkanTombolBeli() {
+    const tombolBeli = document.querySelectorAll('.btn-detail');
+    tombolBeli.forEach(button => {
+        button.addEventListener('click', function(e) {
+            const cardBody = e.target.closest('.card-body');
+            const stokElement = cardBody.querySelector('.stok-text');
+            
+            // Sekarang stokElement sudah bisa ditemukan karena class sudah ada di HTML
+            let stok = parseInt(stokElement.innerText.replace("Stok: ", ""));
+
+            if (stok > 0) {
+                stok--;
+                stokElement.innerText = "Stok: " + stok;
+                const namaBarang = cardBody.querySelector('.card-title').innerText;
+                alert("Berhasil membeli " + namaBarang);
+            } else {
+                alert("Stok Habis!");
+                e.target.disabled = true;
+                e.target.innerText = "Habis";
+            }
+        });
+    });
+}
+
+// Jalankan fungsi
+aktifkanTombolBeli();
